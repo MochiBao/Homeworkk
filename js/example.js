f7b7f1b9f87f1ba50b99b8e80a1ad151eda47ed8
MochiBao <113111181+MochiBao@users.noreply.github.com>
2023-01-10 16:11:12 +0200

    Add files via upload

---
+++ js/example.js
@@ -0,0 +1,7 @@
+const goods = "apples";
+const price = 7;
+const amount = 10;
+const value = price * amount;
+console.log('value :>> ', value);
+const message = `Ви обрали ${goods}, ціна за предмет ${value} гривень`;
+console.log('message :>> ', message);
